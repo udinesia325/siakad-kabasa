603dcbb9b8086097ef9d50b2e9a5e6c8767c1ee5
udinesia325 <udinesia325@gmail.com>
2026-05-18 23:16:05 +0700

    feat(auth): switch routes/sistem.php ke permission middleware

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class RoleController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store()
+    {
+        //
+    }
+
+    public function edit()
+    {
+        //
+    }
+
+    public function update()
+    {
+        //
+    }
+
+    public function destroy()
+    {
+        //
+    }
+}
